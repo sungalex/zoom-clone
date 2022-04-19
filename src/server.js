f3789ba07e5bb4dfeb7086f96d3fe89c6c5ca18d
sungalex <cloudallera@gmail.com>
2022-04-20 03:10:31 +0900

    Chat Completed

--- src/server.js
+++ src/server.js
@@ -14,9 +14,14 @@ const handleListen = () => console.log(`Listening on http://localhost:3000`);
 const server = app.listen(3000, handleListen);
 const wss = new WebSocket.Server({ server });
 
+const sockets = [];
+
 wss.on("connection", (socket) => {
+  sockets.push(socket);
   console.log("Connected to Browser ✅");
   socket.on("close", () => console.log("Disconnected from the Browser ❌"));
-  socket.on("message", (message) => console.log(message.toString()));
+  socket.on("message", (message) => {
+    sockets.forEach((aSocket) => aSocket.send(message.toString()));
+  });
   socket.send("Hello!!!");
 });
