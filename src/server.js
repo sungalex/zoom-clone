6bff0be7ce4d2c98184047531956a96f71f91985
sungalex <cloudallera@gmail.com>
2022-04-20 01:36:19 +0900

    create WebSocket Server

--- src/server.js
+++ src/server.js
@@ -1,12 +1,18 @@
+import http from "http";
 import express from "express";
+import WebSocket from "ws";
 
 const app = express();
 
 app.set("view engine", "pug");
 app.set("views", __dirname + "/views");
 app.use("/public", express.static(__dirname + "/public"));
-app.get("/", (req, res) => res.render("home"));
-app.get("/*", (req, res) => res.redirect("/"));
+app.get("/", (_, res) => res.render("home"));
+app.get("/*", (_, res) => res.redirect("/"));
 
 const handleListen = () => console.log(`Listening on http://localhost:3000`);
+
+const server = http.createServer(app);
+const wss = new WebSocket.Server({ server });
+
 app.listen(3000, handleListen);
