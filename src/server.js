b1e9428ce1bcb4c295ef8579d7cac311f3f5d92c
sungalex <cloudallera@gmail.com>
2022-04-20 02:38:08 +0900

    WebSocket Messages

--- src/server.js
+++ src/server.js
@@ -14,8 +14,9 @@ const handleListen = () => console.log(`Listening on http://localhost:3000`);
 const server = app.listen(3000, handleListen);
 const wss = new WebSocket.Server({ server });
 
-function handleConnection(socket) {
-  console.log(socket);
-}
-
-wss.on("connection", handleConnection);
+wss.on("connection", (socket) => {
+  console.log("Connected to Browser ✅");
+  socket.on("close", () => console.log("Disconnected from the Browser ❌"));
+  socket.on("message", (message) => console.log(message.toString()));
+  socket.send("Hello!!!");
+});
