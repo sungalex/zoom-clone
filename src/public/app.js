32c9768c3781485d117a420d2468746265e6b94c
sungalex <cloudallera@gmail.com>
2022-04-20 22:21:05 +0900

    SocketIO is Amazing

--- src/public/app.js
+++ src/public/app.js
@@ -1,1 +1,15 @@
 const socket = io();
+
+const welcome = document.getElementById("welcome");
+const form = document.querySelector("form");
+
+const handleSubmit = (event) => {
+  event.preventDefault();
+  const input = form.querySelector("input");
+  socket.emit("enter_room", { payload: input.value }, () => {
+    console.log("Server is done");
+  });
+  input.value = "";
+};
+
+form.addEventListener("submit", handleSubmit);
--- src/server.js
+++ src/server.js
@@ -14,7 +14,12 @@ const httpServer = app.listen(3000, handleListen);
 const wsServer = SocketIO(httpServer);
 
 wsServer.on("connection", (socket) => {
-  console.log(socket);
+  socket.on("enter_room", (msg, done) => {
+    console.log(msg);
+    setTimeout(() => {
+      done();
+    }, 10000);
+  });
 });
 
 /* const sockets = [];
