5d1eb592b6f07daafe35946d4654def94499d94c
sungalex <cloudallera@gmail.com>
2022-04-22 06:50:12 +0900

    WebSocket Backup

---
+++ websocket/README.md
@@ -0,0 +1,15 @@
+# Zoom Clone (Noom)
+
+Zoom Clone with NodeJS and Websocket
+
+## Code Challenge - Day1 : WebSocket
+
+- Implement a real time chat using **WebSocket**.
+
+- Put the backend code on `src/server.js` and the frontend code on `src/public/app.js`.
+
+- Allow users to change nickname.
+
+- Display messages sents by other users.
+
+- codsandbox.io : https://codesandbox.io/s/websockets-chat-ur466n
---
+++ websocket/src/server.js
@@ -0,0 +1,45 @@
+import http from "http";
+import WebSocket from "ws";
+import express from "express";
+
+const app = express();
+
+app.set("view engine", "pug");
+app.set("views", __dirname + "/views");
+app.use("/public", express.static(__dirname + "/public"));
+app.get("/", (_, res) => res.render("home"));
+app.get("/*", (_, res) => res.redirect("/"));
+
+const handleListen = () => console.log(`Listening on http://localhost:3000`);
+
+const server = http.createServer(app);
+const wss = new WebSocket.Server({ server });
+
+// Put all your backend code here.
+
+server.listen(process.env.PORT, handleListen);
+
+const sockets = [];
+
+wss.on("connection", (socket) => {
+  sockets.push(socket);
+  socket["nickname"] = "Anonymous";
+  console.log("Connected to Browser ✅");
+  socket.on("close", () => console.log("Disconnected from the Browser ❌"));
+  socket.on("message", (msg) => {
+    const message = JSON.parse(msg.toString());
+    switch (message.type) {
+      case "new_message":
+        sockets.forEach((aSocket) => {
+          if (aSocket !== socket)
+            aSocket.send(`${socket.nickname}: ${message.payload}`);
+        });
+        break;
+      case "nickname":
+        socket["nickname"] = message.payload;
+        break;
+      default:
+        break;
+    }
+  });
+});
