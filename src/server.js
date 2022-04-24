6ba5a792fbe45156644d382c8c38dab7a8f63b0e
sungalex <cloudallera@gmail.com>
2022-04-25 03:54:30 +0900

    WebRTC

--- src/server.js
+++ src/server.js
@@ -12,3 +12,19 @@ const handleListen = () => console.log(`Listening on http://localhost:3000`);
 
 const httpServer = app.listen(3000, handleListen);
 const wsServer = SocketIO(httpServer);
+
+wsServer.on("connection", (socket) => {
+  socket.on("join_room", (roomName) => {
+    socket.join(roomName);
+    socket.to(roomName).emit("welcome");
+  });
+  socket.on("offer", (offer, roomName) => {
+    socket.to(roomName).emit("offer", offer);
+  });
+  socket.on("answer", (answer, roomName) => {
+    socket.to(roomName).emit("answer", answer);
+  });
+  socket.on("ice", (ice, roomName) => {
+    socket.to(roomName).emit("ice", ice);
+  });
+});
